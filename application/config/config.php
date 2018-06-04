cea5c17910e00c530acef2c3a87b652d83de0781
Satria <satriahilda3@gmail.com>
2018-06-04 09:27:17 +0700

    update
    update encrypsi

--- application/config/config.php
+++ application/config/config.php
@@ -330,7 +330,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'akshdajkha13412342askdjfhaidsgh2u129-7581251';
 
 /*
 |--------------------------------------------------------------------------
