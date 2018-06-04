ad451e7a0be424b4fa8e515a2dff6fa328aee7d0
Satria <satriahilda3@gmail.com>
2018-06-04 09:20:53 +0700

    hilda
    update

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = '';
+$config['base_url'] =((isset($_SERVER['HTTPS'])&& $_SERVER['HTTPS']== "on")? "https" : "https");
+$config['base_url'] .= "://". $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"", $_SERVER['SCRIPT_NAME']);
+{
+	$this->foo = $foo;
+}
 
 /*
 |--------------------------------------------------------------------------
