52375f381d259dc2d099c12c7f57f6dece5373e3
Satria <satriahilda3@gmail.com>
2018-06-25 08:13:18 +0700

    undo
    menghilangkan tombol back

--- application/views/About.php
+++ application/views/About.php
@@ -4,7 +4,6 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             <h1>About Coneyco Company</h1>
                 <p>Coneyco Company dibuat oleh M.Alfin Z dia membuat game 2D dengan mekanisme chargetune battle </p>
-                <input type="button" value="Go Back" onclick="history.back(-1)" />
             </div>
     </div>
     </div>
