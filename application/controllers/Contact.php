c37f3b8f411557838d23ce120ae5efcbf9c40c03
Muhammad Alfin Zakariya <alfinyuki@gmail.com>
2018-07-06 23:49:39 +0700

    Contact

---
+++ application/controllers/Contact.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contact extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('kontak');
+	}
+
+}
+
+/* End of file Contact.php */
+/* Location: ./application/controllers/Contact.php */
